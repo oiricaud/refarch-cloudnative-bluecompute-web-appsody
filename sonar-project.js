a43cd08f4584e5ad143fd3c9fcf4df9b3911ac4d
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-03-18 15:27:31 -0500

    update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,17 @@
+const serverUrl = process.env.SONARQUBE_URL;
 const sonarqubeScanner =  require('sonarqube-scanner');
 sonarqubeScanner(
     {
-        serverUrl:  'http://sonarqube-sonarqube-tools.devops-gse-state-458654-0143c5dd31acd8e030a1d6e0ab1380e3-0001.us-east.containers.appdomain.cloud/',
+        serverUrl,
         options : {
+            'sonar.login': process.env.SONARQUBE_USER,
+            'sonar.password': process.env.SONARQUBE_PASSWORD,
             'sonar.sources':  './',
             'sonar.tests':  './',
             'sonar.inclusions'  :  '**', // Entry point of your code
             'sonar.test.inclusions':  'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
             'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
-            'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml'
-        }
+            'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml',
+        },
     }, () => {});
 
