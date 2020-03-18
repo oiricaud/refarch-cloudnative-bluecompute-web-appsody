221124ad8f60e23a600b7ba3fde2ab10e039920b
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-03-18 14:48:40 -0500

    updated the server url in the sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner =  require('sonarqube-scanner');
 sonarqubeScanner(
     {
-        serverUrl:  'http://localhost:9000',
+        serverUrl:  'http://sonarqube-sonarqube-tools.devops-gse-state-458654-0143c5dd31acd8e030a1d6e0ab1380e3-0001.us-east.containers.appdomain.cloud/',
         options : {
             'sonar.sources':  './',
             'sonar.tests':  './',
